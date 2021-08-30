c824262a9d4973aa9c766bf6c89df801a7a3975c
thanbv1510 <thanbv1510@gmail.com>
2021-08-30 23:31:22 +0700

    exercises lec-10

---
+++ lec-10/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img :src="require('/src/assets/images/active-states.jpg')" alt="this is a image">
+    <img src="/lec-10/src/assets/images/active-states.jpg" alt="this is a image">
+    <OrderSummary></OrderSummary>
+  </div>
+</template>
+
+<script>
+import OrderSummary from './components/OrderSummary'
+
+export default {
+  name: 'App',
+  components: {
+    OrderSummary
+  }
+}
+</script>
---
+++ lec-10/src/components/OrderSummary.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="order-summary">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+.order-summary {
+  /*background-image: url("./src/assets/images/pattern-background-desktop.svg");*/
+}
+</style>
